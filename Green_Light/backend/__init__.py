6545b0bfbefd586347d603991dd9e287007a2239
BUbinaJ <bryanjubina@gmail.com>
2021-10-05 11:29:25 -0700

    init

--- Green_Light/backend/__init__.py
+++ Green_Light/backend/__init__.py
@@ -6,32 +6,35 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-def create_website():
-    website = Flask(__name__)
-    website.config['SECRET KEY'] = 'grnlgt'
-    website.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_website(website)
 
-    website.register_blueprint(views, url_prefix='/')
-    website.register_blueprint(auth, url_prefix='/')
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
 
-    create_database(website)
+    create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
-    login_manager.init_website(website)
+    login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-    from .views import views
-    from .auth import auth
-    
-    return website
 
-def create_database(website):
+    return app
+
+
+def create_database(app):
     if not path.exists('website/' + DB_NAME):
-        db.create_all(website=website)
+        db.create_all(app=app)
         print('Created Database!')
